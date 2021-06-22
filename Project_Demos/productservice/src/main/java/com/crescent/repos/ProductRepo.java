4570514ae372ebfafbcc32c167e8684fa453c907
Nirbhay Kumar <35212340+nirbkuma@users.noreply.github.com>
2021-06-22 11:09:31 +0530

    Delete Project_Demos/productservice directory

--- Project_Demos/productservice/src/main/java/com/crescent/repos/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.crescent.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.crescent.entities.Product;
-
-public interface ProductRepo extends JpaRepository<Product, Long> {
-
-}
