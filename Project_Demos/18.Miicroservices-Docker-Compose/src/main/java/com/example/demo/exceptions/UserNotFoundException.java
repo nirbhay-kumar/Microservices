e07cad30327152319c02ff108f53dc8d461e788a
Nirbhay Kumar <35212340+nirbkuma@users.noreply.github.com>
2021-06-23 21:56:08 +0530

    Delete Project_Demos/18.Miicroservices-Docker-Compose directory

--- Project_Demos/18.Miicroservices-Docker-Compose/src/main/java/com/example/demo/exceptions/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.exceptions;
-
-public class UserNotFoundException extends RuntimeException {
-
-    public UserNotFoundException(String name) {
-        super(String.format("username %s not found . ", name));
-    }
-}
